4b9cac38510d1a067b9d6e4d93541ecfd0c658f6
Bean <qaz326978547@gmail.com>
2025-06-13 17:32:52 +0800

    [test] 測試seed 有無正常運作

--- db/seed/createCourseCategoriesSeed.js
+++ db/seed/createCourseCategoriesSeed.js
@@ -3,10 +3,14 @@ const { dataSource } = require('../data-source')
 async function seedCourseCategories() {
   const repo = dataSource.getRepository('course_categories')
   const names = ['all', 'bread', 'cookie', 'cake']
+  console.log('Seeding course categories...')
   for (const name of names) {
     const exist = await repo.findOne({ where: { name } })
     if (!exist) {
       await repo.save({ name })
+      console.log(`Created course category: ${name}`)
+    } else {
+      console.log(`Course category already exists: ${name}`)
     }
   }
 }
