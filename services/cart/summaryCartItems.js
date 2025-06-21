151b8b47dc90a316584e92edc69468b74482b4c7
cyberseris <zihlingchen@gmail.com>
2025-06-22 01:44:07 +0800

    修改購物車資訊

--- services/cart/summaryCartItems.js
+++ services/cart/summaryCartItems.js
@@ -7,8 +7,7 @@ async function summaryCartItems(cartItemsRepo, cart_id){
             'COALESCE(SUM(course.sell_price),0)::int AS total_price' 
             ]) // COALESCE 把 NULL 改成 0，確保 total_price 一定有數字
     .leftJoin('cartItems.courses', 'course')
-    .where('cartItems.cart_id = :cart_id', { cart_id })
-    .where('course.course_status = :course_status', { course_status: '上架'})
+    .where('cartItems.cart_id = :cart_id AND course.course_status = :course_status', { cart_id: cart_id, course_status: '上架'})
     .getRawOne()
 }
 
